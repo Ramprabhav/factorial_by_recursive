a088b47624e1ab29e425a8280532a885234b60bc
RAMPRABHAV SAHANI <146636218+Ramprabhav@users.noreply.github.com>
2024-01-18 16:26:18 -0800

    Update factorial_recursive_calling.java

--- factorial_recursive_calling.java
+++ factorial_recursive_calling.java
@@ -3,8 +3,7 @@ public class factorial_recursive_calling {
     public static int factorial(int num)
     {  if(num==0||num==1)
         return 1;
-
-        return num*factorial(num-1);
+     return num*factorial(num-1);
     }
 public static void main(String[]args)
 {
@@ -16,6 +15,4 @@ public static void main(String[]args)
     System.out.println("the factorial is:"+result);
     sc.close();
 }
-
-    
 }
