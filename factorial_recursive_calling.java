325c390593fd4dd528cb9c3bc4d6350e4e20043b
RAMPRABHAV SAHANI <146636218+Ramprabhav@users.noreply.github.com>
2024-01-18 16:25:14 -0800

    Add files via upload

---
+++ factorial_recursive_calling.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class factorial_recursive_calling {
+    public static int factorial(int num)
+    {  if(num==0||num==1)
+        return 1;
+
+        return num*factorial(num-1);
+    }
+public static void main(String[]args)
+{
+    Scanner sc=new Scanner(System.in);
+    int n;
+    System.out.println("enter the number of which you want to calculate factorial");
+    n=sc.nextInt();
+    int result=factorial(n);
+    System.out.println("the factorial is:"+result);
+    sc.close();
+}
+
+    
+}
